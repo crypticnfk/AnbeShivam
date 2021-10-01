a60335050b1479e7a81f2579871e0ab6422d6d08
Abhinav <abhinavtb@gmail.com>
2021-10-01 19:27:57 +0530

    cleaned up existing ones

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const AnbeShivamMain = artifacts.require("AnbeShivamMain");
-const AnbeShivamInvestorToken = artifacts.require("AnbeShivamInvestorToken");
-const AnbeShivamNFT = artifacts.require("AnbeShivamNFT");
-
-module.exports = function(deployer) {
-  deployer.deploy(AnbeShivamMain);
-  deployer.deploy(AnbeShivamInvestorToken);
-  deployer.deploy(AnbeShivamNFT);
-};
