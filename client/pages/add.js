a14a6b1ec68fcf8fbf50f6b7c149e7576d600282
crypticnfk <89737792+crypticnfk@users.noreply.github.com>
2021-10-11 10:36:21 +0530

    Add new content functional

--- client/pages/add.js
+++ client/pages/add.js
@@ -1,64 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from '../styles/AddContent.module.css'
 import { create } from 'ipfs-http-client';
+import { loadWeb3, loadBlockchainData, addContent } from '../utils/web3-utils';
+
 const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function AddContent() {
 
-    const [inputs, setInputs] = useState({});
-    const [file, updateFile] = useState(``)
-    const [url, updateUrl] = useState(``)
+    useEffect(async() => {
+        const isConnected = await loadWeb3();
+        setConnected(isConnected);
+        if(isConnected) {
+            await loadBlockchainData();
+        }
+    },[]);
 
-    const handleChange = (event) => {
-        
+    const [connected, setConnected] = useState(false);
+    const [inputs, setInputs] = useState({});
+    const [file, updateFile] = useState(``);
+    const [url, updateUrl] = useState(``);
+
+    const handleChange = (event) => {        
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
+        setInputs(values => ({ ...values, [name]: value }));
     }
-    const handleFile = (event) =>{
-   
-        updateFile(event.target.files[0])
+    const handleFile = (event) =>{   
+        updateFile(event.target.files[0]);
     }
     const handleSubmit = async(event) => {
         event.preventDefault();
 
         try {
-            const added = await client.add(file)
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`
-            console.log(url)
-            updateUrl(url)
+            const added = await client.add(file);
+            const pathUrl = `https://ipfs.infura.io/ipfs/${added.path}`;
+            console.log(pathUrl)
+            updateUrl(pathUrl)
         } catch (error) {
             console.log('Error uploading file: ', error)
         }
-        console.log(url)
+        await addContent(inputs.name, url);
     }
-    return (
-        <div className="col-md-6 offset-md-3 mt-5">
 
-            <h1 className={styles.heading}>Add your NFT</h1>
-            <form acceptCharset="UTF-8" method="POST" onSubmit={handleSubmit} target="_blank" className={styles.addMain}>
-                <div className="form-group">
-                    <label htmlFor="exampleInputName">NFT Name</label>
-                    <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="Enter your NFT Name" required="required" onChange={handleChange} />
+    if(connected) {
+        return (
+            <div className="col-md-6 offset-md-3 mt-5">
+
+                <h1 className={styles.heading}>Add a New Project</h1>
+                <form acceptCharset="UTF-8" method="POST" onSubmit={handleSubmit} target="_blank" className={styles.addMain}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputName">Project Name</label>
+                        <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="Enter Project Name" required="required" onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect1">Content Type</label>
+                        <select className="form-control" id="exampleFormControlSelect1" name="platform" required="required" onChange={handleChange}>
+                            <option>Image</option>
+                            <option>Video</option>
+                            <option>Audio</option>
+                        </select>
+                    </div>
+                    <hr />
+                    <div className="form-group mt-3">
+                        <label className="mr-2">Upload Project Content: </label>
+                        &nbsp;&nbsp;    
+                        <input type="file" name="file" onChange={handleFile} />
+                    </div>
+                    <hr />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+        } else {
+            return(
+                <div className="col-md-6 offset-md-3 mt-5">
+                    <h2>Not Connected</h2>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="exampleFormControlSelect1">NFT Type</label>
-                    <select className="form-control" id="exampleFormControlSelect1" name="platform" required="required" onChange={handleChange}>
-                        <option>Image</option>
-                        <option>Video</option>
-                        <option>Audio</option>
-                    </select>
-                </div>
-                <hr />
-                <div className="form-group mt-3">
-                    <label className="mr-2">Upload your file:</label>
-                    <input type="file" name="file" onChange={handleFile} />
-                </div>
-                <hr />
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    );
+            );
+        }
 }
 
 export default AddContent;
--- client/utils/web3-utils.js
+++ client/utils/web3-utils.js
@@ -3,95 +3,139 @@ import AnbeShivamNFT from "../abis/AnbeShivamNFT.json";
 import AnbeShivamMain from "../abis/AnbeShivamMain.json";
 import AnbeShivamInvestorToken from  "../abis/AnbeShivamInvestorToken.json";
 
-export const web3 = new Web3(window.ethereum);
+let web3, AnbeShivam, ASNFT, GODSToken;
 
-const networkId = await web3.eth.net.getId();
-const networkData = AnbeShivamMain.networks[networkId];
-
-const AnbeShivam = new web3.eth.Contract(AnbeShivamMain.abi, networkData.address);
-const AnbeShivamNFT = new web3.eth.Contract(AnbeShivamNFT.abi, networkData.address);
-const GODSToken = new web3.eth.Contract(AnbeShivamInvestorToken.abi, networkData.address);
-
-export const getAccountAddress = async() => {
-    const accounts = await web3.eth.getAccounts();
-    return accounts[0];
+export const loadWeb3 = async () => {
+  try {
+    if (window.ethereum) {
+      web3 = new Web3(window.ethereum);
+      try {
+        await window.ethereum.enable();
+        return true;
+      } catch (err) {
+        console.log('Transaction rejected by user:', err);
+        return false;
+      };
+    } else if (window.web3) {
+        web3 = new Web3(window.web3.currentProvider);
+        return true;
+    } else {
+        window.alert('Non-Ethereum browser detected. Please install MetaMask plugin');
+        return false;
+    };
+  } catch (err) {
+    console.log('Error: ', err);
+    return false;
+  };
 };
 
+export const loadBlockchainData = async() => {
+  const networkId = await web3.eth.net.getId();
+  const asMainData = AnbeShivamMain.networks[networkId];
+  const asNFTData = AnbeShivamNFT.networks[networkId];
+  const asGODSData = AnbeShivamInvestorToken.networks[networkId];
+
+  if(asMainData && asNFTData && asGODSData) {
+    AnbeShivam = new web3.eth.Contract(AnbeShivamMain.abi, asMainData.address);
+    ASNFT = new web3.eth.Contract(AnbeShivamNFT.abi, asNFTData.address);
+    GODSToken = new web3.eth.Contract(AnbeShivamInvestorToken.abi, asGODSData.address);
+  } else {
+    window.alert("The contract was not deployed to the current network");
+  }
+};
+
+export const getAccountAddress = async() => {
+  const accounts = await web3.eth.getAccounts();
+  return accounts[0];
+};
+
+export const checkInvestor = async() => {
+  const account = await getAccountAddress();
+  const bl = await web3.eth.getBalance(account);
+  const balance = await web3.utils.fromWei(bl.toString());
+  const nfts = await getNFTBalance(web3);
+  if(parseFloat(balance) > 10 || nfts > 0) {
+    window.alert("Welcome investor");
+  } else {
+    window.alert("You're not allowed access");
+  }
+}
+
 export const getGODSBalance = async() => {
-    const account = getAccountAddress();
-    const result = await GODSToken.methods.balanceOf(account).call();
-    const balance = web3.utils.fromWei(result);
-    return balance;
+  const account = await getAccountAddress();
+  const result = await GODSToken.methods.balanceOf(account).call();
+  const balance = web3.utils.fromWei(result);
+  return balance;
 };
 
 export const getNFTBalance = async() => {
-    const account = getAccountAddress();
-    const balance = await AnbeShivamNFT.methods.balanceOf(account).call();
-    return balance;
+  const account = await getAccountAddress();
+  const balance = await ASNFT.methods.balanceOf(account).call();
+  return balance;
 };
 
 export const getTokenURI = async(tokenId) => {
-    const uri = await AnbeShivamNFT.methods.tokenURI(tokenId).call();
-    return uri;
+  const uri = await ASNFT.methods.tokenURI(tokenId).call();
+  return uri;
 };
 
 export const addContent = async(projectName, fileURL) => {
-    const account = getAccountAddress();
-    await AnbeShivam.methods
-      .addContent(projectName, fileURL)
-      .send({
-        from: account
-      })
-      .on("transactionHash", function (hash) {})
-      .on("receipt", function (receipt) {})
-      .on("confirmation", (confirmationNumber, receipt) => {
-        window.alert("Project was added successfully");
-      })
-      .on("error", (error, receipt) => {
-        window.alert("Error occured: ", error);
-      });
-  
-    window.location.reload();
+  const account = await getAccountAddress();
+  await AnbeShivam.methods
+    .addContent(projectName, fileURL)
+    .send({
+      from: account
+    })
+    .on("transactionHash", function (hash) {})
+    .on("receipt", function (receipt) {})
+    .on("confirmation", (confirmationNumber, receipt) => {
+      window.alert("Project was added successfully");
+    })
+    .on("error", (error, receipt) => {
+      window.alert("Error occured: ", error);
+    });
+
+  window.location.reload();
 };
 
 export const returnContent = async(projectId) => {
-    const account = getAccountAddress();
-    await AnbeShivam.methods
-      .returnContent(projectId)
-      .send({
-        from: account
-      })
-      .on("transactionHash", function (hash) {})
-      .on("receipt", function (receipt) {})
-      .on("confirmation", (confirmationNumber, receipt) => {        
-        const result = await web3.eth.getTransactionReceipt(receipt.transactionHash);
-        const event = result.logs[0].topics[0];
-        return event[0];
-      })
-      .on("error", (error, receipt) => {
-        window.alert("Error occured while accessing content");
-      });
+  const account = await getAccountAddress();
+  await AnbeShivam.methods
+    .returnContent(projectId)
+    .send({
+      from: account
+    })
+    .on("transactionHash", function (hash) {})
+    .on("receipt", function (receipt) {})
+    .on("confirmation", (confirmationNumber, receipt) => {        
+      const result = web3.eth.getTransactionReceipt(receipt.transactionHash);
+      const event = result.logs[0].topics[0];
+      return event[0];
+    })
+    .on("error", (error, receipt) => {
+      window.alert("Error occured while accessing content");
+    });
 };
 
 export const returnProjectCount = async() => {
-    const projectCount = await AnbeShivam.methods.returnContentCount().call();
-    return projectCount;
+  const projectCount = await AnbeShivam.methods.returnContentCount().call();
+  return projectCount;
 };
 
 export const investFunds = async(contentId, metadata, amount) => {
-    const account = getAccountAddress();
-    await AnbeShivam.methods
-      .investFunds(contentId, metadata)
-      .send({
-        from: account,
-        value: web3.utils.toWei(amount.toString())
-      })
-      .on("transactionHash", function (hash) {})
-      .on("receipt", function (receipt) {})
-      .on("confirmation", (confirmationNumber, receipt) => {
-        window.alert("Successfully funded project " + contentId.toString());
-      })
-      .on("error", (error, receipt) => {
-        window.alert("Error occured while accessing content");
-      });
+  const account = await getAccountAddress();
+  await AnbeShivam.methods
+    .investFunds(contentId, metadata)
+    .send({
+      from: account,
+      value: web3.utils.toWei(amount.toString())
+    })
+    .on("transactionHash", function (hash) {})
+    .on("receipt", function (receipt) {})
+    .on("confirmation", (confirmationNumber, receipt) => {
+      window.alert("Successfully funded project " + contentId.toString());
+    })
+    .on("error", (error, receipt) => {
+      window.alert("Error occured while accessing content");
+    });
 };
