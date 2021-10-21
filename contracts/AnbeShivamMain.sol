683220e15b11efec8b2078dcc6b01ab1b6727f9c
crypticnfk <89737792+crypticnfk@users.noreply.github.com>
2021-10-21 22:06:37 +0530

    Minor fix in contract

--- contracts/AnbeShivamMain.sol
+++ contracts/AnbeShivamMain.sol
@@ -56,6 +56,7 @@ contract AnbeShivamMain is Ownable {
 
     function investFunds(uint _contentID, string memory _metadata) external payable {
         require(msg.value > 0 && msg.value <= 1000000 ether, "Invalid amount");
+        require(msg.sender != contents[_contentID].creator, "Creator cannot invest");
         contents[_contentID].receivedFunds += msg.value;
         contents[_contentID].creator.transfer(msg.value);
         AnbeShivamInvestorToken(GODSContract).mint(msg.sender, msg.value);
